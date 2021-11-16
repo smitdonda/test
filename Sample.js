da9310e965001e31d7416e0f2be9f04f31d84a27
smitdonda <94434466+smitdonda@users.noreply.github.com>
2021-11-16 13:13:33 +0530

    Add files via upload

---
+++ Sample.js
@@ -0,0 +1,29 @@
+var key  = {
+    "Memory_Summary" : {
+        "Maximum_Capacity" : "2048 Bytes",
+        "Maximum_Memory_Module_Size" : "4096 Bytes",
+        "Memory_Slots" : 4,
+        "Error_Correction" : "None",
+        "DRAM_Frequency" : "199.MHz",
+        "Memory_Timings" : "3-3-3-8(CL-RCD-RP-RAS)"
+    },
+    
+    "Device_Locator" : [{
+        "slot1" : {
+            "Manufacture" : "Hyundai Electronics",
+            "Part_Number" : "HYMD264 646D8J-D43",
+            "Serial_Number" : "FFFF7240",
+            "Capacity" : "512MBytes",
+            "Memory_Type" : "DDR (PC3200)",
+            "Speed" : "200 MHz",
+            "Supported_Frequencies" : "133MHz,166 MHz,200 MHz",
+            "Memory_Timings" : "2-2-2-6-0 at 133 MHz, at 2",
+            "Memory_Timings" : "2-3-3-7-0 at 166 MHz, at 2",
+            "Memory_Timings" : "3-3-3-8-0 at 200 MHz, at 2",
+            "Data_Width" : "64 bits",
+            "Manufacturing_Date" : "2005 , Week 39",
+            "EPP_SPD Support" : "No",
+            "XMP_SPD Support" : "No"
+        }
+    }]
+}
